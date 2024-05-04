93cb917583eaaf781d5ff84903a70ac17c8a487d
Tanatorn Suteerapornchai <132776555+atnts@users.noreply.github.com>
2024-05-04 11:37:09 +0700

    edit oage

--- src/app/_components/mainSideBar.tsx
+++ src/app/_components/mainSideBar.tsx
@@ -1,7 +1,31 @@
+import SideBarButton from "@/components/sidebarbutton";
+import SideBarCollapsible from "@/components/sidebarcollapsible";
+import { AiOutlinePushpin } from "react-icons/ai";
+import { BiHive, BiHome, BiRocket } from "react-icons/bi";
+
 const MainSideBar = async () => {
 	return (
 		<>
-			<nav className="sticky overflow-scroll left-0 pr-6">sidebar</nav>
+			<nav className="sticky overflow-scroll left-0 pr-6">
+			<SideBarButton
+					Icon={() => <BiHome />}
+					title="Home"
+				/>
+				<SideBarButton
+					Icon={() => <BiRocket />}
+					title="Trending"
+				/>
+				<hr className="my-2 border-t border-gray" />
+				<SideBarCollapsible
+					Icon={() => <BiHive />}
+					title="Communities"
+				/>
+				<hr className="my-2 border-t border-gray" />
+				<SideBarCollapsible
+					Icon={() => <AiOutlinePushpin />}
+					title="Topics"
+				/>
+			</nav>
 		</>
 	);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import { axios } from "@/libs/axiosInstance";
-import { cn } from "@dookdiks/utils";
-
 export default function Home() {
-	const formAction = async (formData: FormData) => {
-		"use server";
 
-		const studentid = formData.get("studentid");
-		try {
-			const token = await axios.post("api/auth/change-password-ticket", {
-				data: studentid,
-			});
-			console.log(token.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
 
 	return (
-		<main className={cn("")}>
-			<form action={formAction}>
-				<input type="text" name="studentid" />
-				<button type="submit">sent</button>
-			</form>
-		</main>
+		<div>
+			kjsdklajdkl
+		</div>
 	);
 }
