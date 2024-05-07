ba8674bbaad39f1f4faf5917ef92611b47b70d7a
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-05-08 06:00:03 +0700

    Refactor topic page to use getTopicsPost utility function

--- src/libs/utils/topics/index.ts
+++ src/libs/utils/topics/index.ts
@@ -1,30 +1,46 @@
 import { axios } from '@/libs/axiosInstance';
 import { ReturnResponse } from '@/types/ResponseType';
-import { TopicSchema } from '@/types/zodSchema';
+import {
+  CommentSchema,
+  CommunitiesSchema,
+  PostSchema,
+  TopicSchema,
+  UserSchema,
+} from '@/types/zodSchema';
 import { z } from 'zod';
 
-type TopicsType = z.infer<typeof TopicSchema>;
+type TopicType = z.infer<typeof TopicSchema>;
 
-type GetTopicsProps = Partial<TopicsType>;
+type GetTopicsProps = Partial<TopicType>;
+
+type PostType = z.infer<typeof PostSchema>;
+
+export type TopicsType = z.infer<typeof TopicSchema> & {
+  user: z.infer<typeof UserSchema>;
+  PostTopic: z.infer<typeof PostSchema>[];
+  comments: z.infer<typeof CommentSchema>[];
+  communities: z.infer<typeof CommunitiesSchema> | null;
+  topics: z.infer<typeof TopicSchema>[];
+};
 
 export const getTopics = async (props: GetTopicsProps) => {
   try {
     const res = await axios.get<
-      TopicsType[],
-      ReturnResponse<{ data: TopicsType[] }>
+      TopicType[],
+      ReturnResponse<{ data: TopicType[] }>
     >('api/topic', {
       params: props,
     });
 
     return res.data?.data;
   } catch (error: any) {
-    return error.response.data as TopicsType[];
+    return error.response.data as TopicType[];
   }
 };
 
-export const createTopic = async (props: Pick<TopicsType, 'name'>) => {
+export const createTopic = async (props: Pick<TopicType, 'name'>) => {
   try {
-    const res = await axios.post<TopicsType, ReturnResponse<TopicsType>>(
+    const res = await axios.post<TopicType, ReturnResponse<TopicType>>(
       'api/topic',
       {
         data: props,
@@ -32,6 +48,27 @@ export const createTopic = async (props: Pick<TopicsType, 'name'>) => {
     );
     return res;
   } catch (error: any) {
-    return error.response.data as ReturnResponse<TopicsType>;
+    return error.response.data as ReturnResponse<TopicType>;
+  }
+};
+
+type TopicPost = TopicType & {
+  post: PostType[];
+};
+
+export const getTopicsPost = async (id: string) => {
+  try {
+    const res = await axios.get<
+      TopicPost,
+      ReturnResponse<{ data: TopicsType[] }>
+    >('api/topic/post', {
+      params: {
+        id,
+      },
+    });
+
+    return res.data?.data;
+  } catch (error: any) {
+    return error.response.data as TopicsType[];
   }
 };
