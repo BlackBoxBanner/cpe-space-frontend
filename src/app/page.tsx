dac14cf103981902b6f4e89406662463648205a9
Tanatorn Suteerapornchai <132776555+atnts@users.noreply.github.com>
2024-05-08 05:42:04 +0700

    change main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,12 @@
-import { Button, Link } from '@/components/common/button';
-import { axios } from '@/libs/axiosInstance';
-import { signout } from '@/libs/utils/auth/signout';
-import { redirect } from 'next/navigation';
 import CreatePost from './_components/createpost';
 import PostBox from './_components/mainpostbox.tsx/postbox';
 export default async function Home() {
-  const formAction = async (formData: FormData) => {
-    'use server';
 
-    const studentid = formData.get('studentid');
-    try {
-      const token = await axios.post('api/auth/change-password-ticket', {
-        data: studentid,
-      });
-      console.log(token.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const signOutFormAction = async (formData: FormData) => {
-    'use server';
-    await signout();
-    redirect('/');
-  };
   return (
     <div className="flex flex-col gap-6">
       <CreatePost />
       <hr className="my-2 border-t border-gray" />
       <PostBox />
-      <form action={signOutFormAction}>
-        <Button type="submit">Signout</Button>
-      </form>
     </div>
   );
 }
