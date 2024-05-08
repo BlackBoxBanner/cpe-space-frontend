2f23b022265eaad625cde74d010353a684b48958
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-05-08 13:47:07 +0700

    Refactor Trending page to remove CreatePost component and update async function declaration

--- src/app/trending/page.tsx
+++ src/app/trending/page.tsx
@@ -1,9 +1,8 @@
 import { axios } from '@/libs/axiosInstance';
 import { Post, PostType } from '@/components/common/post';
 import { ReturnResponse } from '@/types/ResponseType';
-import CreatePost from '../_components/createpost';
 
-export default async function Trending() {
+async function Trending() {
   const postData = await axios.get<ReturnResponse<PostType[]>>('/api/post');
 
   if (postData.data.error) throw new Error('Error fetching post data');
@@ -16,4 +15,5 @@ export default async function Trending() {
     </div>
   );
 }
+
 export default Trending;
